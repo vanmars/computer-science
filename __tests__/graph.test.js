85edf90b8e92291639868c3b080117d241ada275
Vanessa Stewart <vstewart1213@gmail.com>
2021-01-12 09:57:04 -0800

    Add method and pass test for establishing edge between two nodes

--- __tests__/graph.test.js
+++ __tests__/graph.test.js
@@ -21,8 +21,17 @@ test('should return null if the node does not exist in the adjacency list', () =
     expect(graph.hasNode("Ada")).toEqual(false);
   })
 
-  test('check to see if node exists in graph', () => {
+test('check to see if node exists in graph', () => {
     graph.addNode("Jasmine");
     expect(graph.hasNode("Jasmine")).toEqual(true);
   })
+
+test('add an edge between two nodes', () => {
+    graph.addNode("Jasmine");
+    graph.addNode("Ada");
+    graph.createEdge("Jasmine", "Ada");
+    expect(graph.adjacencyList.get("Jasmine").has("Ada")).toEqual(true);
+    expect(graph.adjacencyList.get("Ada").has("Jasmine")).toEqual(true);
+  });
+
 })
--- src/graph.js
+++ src/graph.js
@@ -13,5 +13,10 @@ export default class Graph {
     }
     return false;
   }
+
+  createEdge(node1, node2) {
+    this.adjacencyList.get(node1).add(node2);
+    this.adjacencyList.get(node2).add(node1);
+  }
   
 }
