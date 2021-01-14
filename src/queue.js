3d24644132a260b071b1f67abec3075eb46127ac
Vanessa Stewart <vstewart1213@gmail.com>
2021-01-14 10:41:00 -0800

    Add a stack and queue class

---
+++ src/queue.js
@@ -0,0 +1,14 @@
+class Queue {
+  constructor() {
+    this.elements = [];
+  }
+
+  enqueue (element) {
+    return this.elements.push(elements);
+  }
+
+  dequeue () {
+    return this.elements.shift();
+  }
+
+}
---
+++ src/stack.js
@@ -0,0 +1,14 @@
+class Stack {
+  constructor() {
+    this.elements = [];
+  }
+
+  push(element) {
+    return this.elements.push(elements);
+  }
+
+  pop() {
+    return this.elements.pop();
+  }
+
+}
